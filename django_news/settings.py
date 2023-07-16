c7bb677d68a94c67f8084d26cac09cc9d2ce0956
Serdar Güner <serdarguner@gmail.com>
2023-07-16 16:35:22 +0300

    jason data upload

--- django_news/settings.py
+++ django_news/settings.py
@@ -76,6 +76,10 @@ WSGI_APPLICATION = 'django_news.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+      'defaultx': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'URL': 'postgresql://postgres:FFwNt28m2tnWGodbkeRe@containers-us-west-64.railway.app:7869/railway',
